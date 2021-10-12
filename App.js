eee2ed5f547b118e7be3a97593d5084278ce36f6
MarianeCaldeira <87836763+MarianeCaldeira@users.noreply.github.com>
2021-10-12 11:02:10 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import {Mensagem} from './Mensagem';
+
+export const App = () => (
+    <div className="App">
+        <Mensagem/>
+    </div>
+)
---
+++ Mensagem.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export const Mensagem = () => <p>Otimismo é a chave, motivação a porta, e potencial o caminho para a grande<br/>
+conquista que se chama SONHO.
+</p>
